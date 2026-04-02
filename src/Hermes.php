a3511099b215009f87e6aeaaa5edc216e397595b
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-02 08:40:48 +0200

    fix: Support both old-style Horde_Variables and modern Horde\Util\Variables

---
+++ src/Hermes.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright 2013-2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD-2-Clause). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
+ *
+ * @author   Michael J Rubinsky <mrubinsk@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsdl.php BSD-2-Clause
+ * @package  Hermes
+ */
+
+declare(strict_types=1);
+
+namespace Horde\Hermes;
+
+/**
+ * Hermes application class.
+ *
+ * @author    Michael J Rubinsky <mrubinsk@horde.org>
+ * @category  Horde
+ * @copyright 2013-2026 The Horde Project
+ * @license   http://www.horde.org/licenses/bsdl.php BSD-2-Clause
+ * @package   Hermes
+ */
+class Hermes
+{
+}
